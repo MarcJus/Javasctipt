5c3d6b9eccd354e7fa22ea3535dff6f8c1f3ad01
MarcJus <jusseaumemarc@gmail.com>
2020-08-30 11:59:06 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var cntx = document.querySelector("#cntnr");
+window.addEventListener("contextmenu", function(e){
+	e.preventDefault();
+	cntx.style.top = e.offsetY + "px";
+	cntx.style.left = event.offsetX + "px";
+	cntx.classList.add("active");
+});
+
+window.addEventListener("click", function(e){
+	var style = cntx.style;
+	var rect = cntx.getBoundingClientRect();
+	var top = rect.top;
+	var height = rect.height;
+	var left = rect.left;
+	var width = rect.width;
+	if(e.offsetY < top){
+		cntx.classList.remove("active");
+		return;
+	} else if(e.offsetY > top+height){
+		cntx.remove("active");
+		return;
+	}
+});
